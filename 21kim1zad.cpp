415927fb4b9dbd5e3243adc14e9c090ad867499b
SquidwardOV <115181787+SquidwardOV@users.noreply.github.com>
2023-06-29 14:00:21 +0300

    Update 21kim1zad.cpp

--- 21kim1zad.cpp
+++ 21kim1zad.cpp
@@ -32,7 +32,7 @@ int main() {
 
     std::vector<int> result;
     for (int i = 0; i < n; i++) {
-        if (isMultipleOfThree(numbers[i]) && numbers[i] <= minElement) {
+        if (isMultipleOfThree(numbers[i])) {
             result.push_back(numbers[i]);
         }
     }
